ab41d563735238c8a2119d7669d4ca162719e235
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-23 16:30:44 -0600

    debug

---
+++ src/debug/index.js
@@ -0,0 +1,9 @@
+var a = 'Hello'
+
+function hello() {
+    let b = 'hello world'
+    const c = 'Hello World!'
+    debugger;
+}
+
+hello();
---
+++ src/hoisting/index.js
@@ -0,0 +1,10 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo'
+// console.log(nameOfDog);
+
+nameOfDog()
+var elmo = 'Elmito'
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`)
+}
+
