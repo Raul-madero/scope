58e627765e5e4fb31b9002311a86c60b7fba6ef8
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-23 12:25:52 -0600

    Closures

---
+++ src/closure/closure.js
@@ -0,0 +1,24 @@
+//Permiten acceder al ámbito de una función exterior desde una función interior. En JavaScript, las clausuras se crean cada vez que una función es creada.
+//A diferencia de otros conceptos los closures no son siempre utilizados.
+// Aprender el concepto es saber identificar cuando los estás utilizando.
+// Tienes una closure cuando una función cualquiera accede a una variable fuera de su contexto y la recuerda.
+
+const myGlobal = 0
+
+function myFunction() {
+    const myNumber = 1
+    console.log(myGlobal);
+
+    function parent() { // funcion interna
+        const inner = 2
+        console.log(myNumber, myGlobal);
+
+        function child() {
+            console.log(inner, myNumber, myGlobal);
+        }
+        return child()
+    }
+    return parent()
+}
+
+myFunction()
